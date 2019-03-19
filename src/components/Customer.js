d832083a09f94293e9a29b4bbaddbaf81ee2c914
hwi <khdrogba@gmail.com>
2019-03-19 21:38:28 +0900

    apply material ui

--- src/components/Customer.js
+++ src/components/Customer.js
@@ -1,43 +1,22 @@
 import React from 'react';
+import TableRow from '@material-ui/core/TableRow';
+import TableCell from '@material-ui/core/TableCell';
 
 //라이브러리
 class Customer extends React.Component{
     //항상 수행, customer를 실제로 그리는내용을담음
     render(){
-        return(
-            <div>
-                
-                <CustomerProfile id={this.props.id} image={this.props.image} name={this.props.name}/>   
-                <CustomerInfo birthday={this.props.birthday} gender={this.props.gender} job={this.props.job}/>
-            </div>
+        return(                        
+            <TableRow>
+                <TableCell>{this.props.id}</TableCell>
+                <TableCell><img src={this.props.image} alt="profile"/></TableCell>
+                <TableCell>{this.props.name}</TableCell>  
+                <TableCell>{this.props.birthday}</TableCell>  
+                <TableCell>{this.props.gender}</TableCell>
+                <TableCell>{this.props.job}</TableCell>                    
+            </TableRow>            
         )
         
     }
 }
-class CustomerProfile extends React.Component{
-    render(){
-        return(
-            <div>
-                {/* 이걸 부른놈의 id,name,,, */}
-                <h2>{this.props.name}({this.props.id})</h2>
-                <img src={this.props.image} alt="profile"/>
-
-            </div>
-        )
-    }     
-    
-}
-
-class CustomerInfo extends React.Component{
-    render(){
-        return(
-            <div>
-                <p>{this.props.birthday}</p>
-                <p>{this.props.gender}</p>
-                <p>{this.props.job}</p>
-            </div>
-        )
-    }
-}
-
 export default Customer;
